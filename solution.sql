f637f1aa19310a2f3451d216c28667e7c467be08
verydnobl337 <verydnob@yandex.ru>
2026-01-03 08:42:21 -0400

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -2,132 +2,156 @@
 
 -- Создание схемы raw_data:
 create schema if not exists raw_data;
-
 -- Создание таблицы sales в raw_data:
 create table if not exists raw_data.sales (
-	id INTEGER,
-	auto TEXT,
-	gasoline_consumption NUMERIC,
-	price NUMERIC,
-	date DATE,
-	person_name TEXT,
-	phone TEXT,
-	discount INTEGER,
-	brand_origin TEXT
+    id integer,
+    auto text,
+    gasoline_consumption numeric,
+    price numeric,
+    date date,
+    person_name text,
+    phone text,
+    discount integer,
+    brand_origin text
 );
-
 -- Заполняю таблицу sales данными через psql:
 \copy raw_data.sales(id, auto, gasoline_consumption, price, date, person_name, phone, discount, brand_origin) FROM 'C:/Users/adond/AppData/Local/Temp/cars.csv' DELIMITER ',' CSV HEADER NULL 'null';
-
 -- Создание схемы car_shop:
 create schema if not exists car_shop;
-
+-- Создание таблицы countries:
+create table car_shop.countries (
+    id serial primary key,
+    country_name text not null unique
+);
+-- Заполняю таблицу countries:
+insert into car_shop.countries (country_name)
+/* Обработаю также "пустые" cтраны */
+select distinct coalesce(brand_origin, 'Unknown')
+from raw_data.sales s
+where not exists (
+    select 1 from car_shop.countries c
+    where c.country_name = coalesce(s.brand_origin, 'Unknown')
+);
+-- Создание таблицы brands:
+create table car_shop.brands (
+    id serial primary key,
+    brand_name text not null unique,
+    country_id integer not null references car_shop.countries(id)
+);
+-- Заполняю таблицу brands:
+insert into car_shop.brands (brand_name, country_id)
+/* Обработаю также "пустые" бренды */
+select distinct
+    coalesce(split_part(trim(split_part(s.auto, ',', 1)), ' ', 1), 'Unknown'),
+    c.id
+from raw_data.sales s
+join car_shop.countries c
+    on c.country_name = coalesce(s.brand_origin, 'Unknown')
+where not exists (
+    select 1 from car_shop.brands b
+    where b.brand_name = coalesce(split_part(trim(split_part(s.auto, ',', 1)), ' ', 1), 'Unknown')
+);
 -- Создание таблицы cars:
 create table car_shop.cars (
-    id SERIAL primary key, /* Автоинкремент, уникальный идентификатор машины */
-    brand VARCHAR not null, /* Название бренда может содержать буквы и цифры */
-    model VARCHAR not null, /* Модель может содержать буквы и цифры */
-    gasoline_consumption NUMERIC(5,2), /* Расход топлива с точностью до сотых, может быть NULL */
-    price NUMERIC(9,2) not null /* Цена до 9 цифр с двумя знаками после запятой */
+    id serial primary key,
+    brand_id integer not null references car_shop.brands(id),
+    model text not null,
+    gasoline_consumption numeric(5,2)
 );
-
+-- Заполняю таблицу cars:
+insert into car_shop.cars (brand_id, model, gasoline_consumption)
+select distinct
+    b.id,
+    substr(
+        trim(split_part(s.auto, ',', 1)),
+        length(split_part(trim(split_part(s.auto, ',', 1)), ' ', 1)) + 2
+    ),
+    s.gasoline_consumption
+from raw_data.sales s
+join car_shop.brands b
+    on b.brand_name = coalesce(split_part(trim(split_part(s.auto, ',', 1)), ' ', 1), 'Unknown')
+where not exists (
+    select 1 from car_shop.cars c
+    join car_shop.brands b2 on b2.id = c.brand_id
+    where c.model = substr(trim(split_part(s.auto, ',', 1)), length(split_part(trim(split_part(s.auto, ',', 1)), ' ', 1)) + 2)
+      and b2.id = b.id
+);
 -- Создание таблицы colors:
 create table car_shop.colors (
-	id SERIAL primary key, /* Автоинкремент, уникальный идентификатор цвета */
-	color_name VARCHAR not null unique /* Название цвета, уникальное значение */
+    id serial primary key,
+    color_name text not null unique
 );
-
--- Создание таблицы car_colors со связью многие-ко-многим:
-create table car_shop.car_colors (
-	car_id INTEGER not null references car_shop.cars(id) on delete cascade, /* При удалении машины удаляются её цвета в car_colors */
-	color_id INTEGER not null references car_shop.colors(id) on delete cascade, /* При удалении цвета удаляются связанные записи в car_colors */
-	primary key (car_id, color_id) /* Первичный ключ для уникальной пары */
-);
-
--- Создание таблицы customers:
-create table car_shop.customers (
-	id SERIAL primary key, /* Автоинкремент, уникальный идентификатор покупателя */
-	name VARCHAR not null, /* Имя покупателя */
-	phone VARCHAR(50) unique null /* Телефон, может быть NULL */
-);
-
--- Создание таблицы sales_clean:
-create table car_shop.sales_clean (
-	id SERIAL primary key, /* Автоинкремент, уникальный идентификатор продажи */
-	car_id INTEGER not null references car_shop.cars(id), /* Ссылка на машину */
-	customer_id INTEGER not null references car_shop.customers(id), /* Ссылка на покупателя */
-	date DATE not null, /* Дата продажи */
-	discount INTEGER /* Скидка, может быть NULL */
-);
-
--- Заполняю таблицу cars:
-insert into car_shop.cars (brand, model, gasoline_consumption, price)
-select distinct /* Уникальные машины */
-	-- В raw_data.sales поле auto содержит марку, модель и цвет разделяем их: 
-	split_part(trim(split_part(auto, ',', 1)), ' ', 1) as brand,
-    substr(trim(split_part(auto, ',', 1)), length(split_part(trim(split_part(auto, ',', 1)), ' ', 1)) + 2) as model,
-    gasoline_consumption,
-    price
-from raw_data.sales;
-
 -- Заполняю таблицу colors:
 insert into car_shop.colors (color_name)
-/* Достаем цвет из поля auto */
-select distinct trim(split_part(auto, ',', 2)) as color_name
-from raw_data.sales
-where auto like '%,%';
-
+/* Обработаю также "пустые" цвета */
+select distinct coalesce(trim(split_part(auto, ',', 2)), 'Unknown')
+from raw_data.sales s
+where not exists (
+    select 1 from car_shop.colors c
+    where c.color_name = coalesce(trim(split_part(s.auto, ',', 2)), 'Unknown')
+);
+-- Создание таблицы car_colors:
+create table car_shop.car_colors (
+    id serial primary key,
+    car_id integer not null references car_shop.cars(id) on delete cascade,
+    color_id integer not null references car_shop.colors(id),
+    unique (car_id, color_id)
+);
 -- Заполняю таблицу car_colors:
 insert into car_shop.car_colors (car_id, color_id)
-select DISTINCT
+select distinct
     c.id,
     co.id
 from raw_data.sales s
-join car_shop.cars c 
-    on c.brand = split_part(trim(split_part(s.auto, ',', 1)), ' ', 1)
-   and c.model = substr(
-        trim(split_part(s.auto, ',', 1)),
-        length(split_part(trim(split_part(s.auto, ',', 1)), ' ', 1)) + 2
-   )
-   and (
-        c.gasoline_consumption = s.gasoline_consumption
-        or (c.gasoline_consumption IS NULL and s.gasoline_consumption IS NULL)
-   )
-   and c.price = s.price 
-join car_shop.colors co 
-    on co.color_name = trim(split_part(s.auto, ',', 2))
-where s.auto LIKE '%,%';
-
+join car_shop.cars c
+    on c.model = substr(trim(split_part(s.auto, ',', 1)), length(split_part(trim(split_part(s.auto, ',', 1)), ' ', 1)) + 2)
+join car_shop.colors co
+    on co.color_name = coalesce(trim(split_part(s.auto, ',', 2)), 'Unknown')
+where not exists (
+    select 1 from car_shop.car_colors cc
+    where cc.car_id = c.id and cc.color_id = co.id
+);
+-- Создание таблицы customers:
+create table car_shop.customers (
+    id serial primary key,
+    name text not null,
+    phone text unique
+);
 -- Заполняю таблицу customers:
 insert into car_shop.customers (name, phone)
-select distinct 
-	person_name as name,
-	phone
-from raw_data.sales;
-
+select distinct person_name, phone
+from raw_data.sales s
+where not exists (
+    select 1 from car_shop.customers cu
+    where cu.name = s.person_name and (cu.phone = s.phone or (cu.phone is null and s.phone is null))
+);  
+-- Создание таблицы sales_clean:
+create table car_shop.sales_clean (
+    id serial primary key,
+    car_color_id integer not null references car_shop.car_colors(id),
+    customer_id integer not null references car_shop.customers(id),
+    sale_date date not null,
+    price numeric(9,2) not null,
+    discount integer check (discount between 0 and 100)
+);
 -- Заполняю таблицу sales_clean:
-insert into car_shop.sales_clean (car_id, customer_id, date, discount)
+insert into car_shop.sales_clean (car_color_id, customer_id, sale_date, price, discount)
 select
-    c.id,
+    cc.id,
     cu.id,
     s.date,
+    s.price,
     s.discount
 from raw_data.sales s
 join car_shop.cars c
-    on c.brand = split_part(trim(split_part(s.auto, ',', 1)), ' ', 1)
-   and c.model = substr(
-        trim(split_part(s.auto, ',', 1)),
-        length(split_part(trim(split_part(s.auto, ',', 1)), ' ', 1)) + 2
-   )
-   and (
-        c.gasoline_consumption = s.gasoline_consumption
-        or (c.gasoline_consumption IS NULL and s.gasoline_consumption IS NULL)
-   )
-   and c.price = s.price
+    on c.model = substr(trim(split_part(s.auto, ',', 1)), length(split_part(trim(split_part(s.auto, ',', 1)), ' ', 1)) + 2)
+join car_shop.colors co
+    on co.color_name = coalesce(trim(split_part(s.auto, ',', 2)), 'Unknown')
+join car_shop.car_colors cc
+    on cc.car_id = c.id and cc.color_id = co.id
 join car_shop.customers cu
     on cu.name = s.person_name
-   and (cu.phone = s.phone or (cu.phone IS NULL and s.phone IS NULL));
-
+   and (cu.phone = s.phone or (cu.phone is null and s.phone is null));
 
 -- Этап 2. Создание выборок
 
@@ -139,54 +163,60 @@ select
     ) AS nulls_percentage_gasoline_consumption
 from car_shop.cars
 where gasoline_consumption IS NULL;
-
 ---- Задание 2. Напишите запрос, который покажет название бренда и среднюю цену его автомобилей в разбивке по всем годам с учётом скидки.
 select 
-	c.brand brand_name,
-	EXTRACT(year from sc.date) sale_year,
-	ROUND(AVG(c.price * (1 - COALESCE(sc.discount, 0)/100.0)), 2) AS price_avg
-from car_shop.cars c
-join car_shop.sales_clean sc on (c.id = sc.car_id)
+    b.brand_name,
+    extract(year from sc.sale_date) as sale_year,
+    round(avg(sc.price), 2) as price_avg
+from car_shop.sales_clean sc
+join car_shop.car_colors cc on cc.id = sc.car_color_id
+join car_shop.cars c on c.id = cc.car_id
+join car_shop.brands b on b.id = c.brand_id
 group by 
-	c.brand,
-	EXTRACT(year from sc.date)
+    b.brand_name,
+    extract(year from sc.sale_date)
 order by
-	c.brand,
-	sale_year;
-
+    b.brand_name,
+    sale_year;
 ---- Задание 3. Посчитайте среднюю цену всех автомобилей с разбивкой по месяцам в 2022 году с учётом скидки.
 select
-	EXTRACT(month from sc.date) as month,
-	EXTRACT(year from sc.date) as sale_year,
-	ROUND(AVG(c.price * (1 - COALESCE(sc.discount, 0)/100.0)), 2) AS price_avg
-from car_shop.cars c
-join car_shop.sales_clean sc on (c.id = sc.car_id)
-where EXTRACT(year from sc.date) = '2022'
+    extract(month from sc.sale_date) as month,
+    round(avg(sc.price), 2) as price_avg
+from car_shop.sales_clean sc
+join car_shop.car_colors cc on cc.id = sc.car_color_id
+join car_shop.cars c on c.id = cc.car_id
+where extract(year from sc.sale_date) = 2022
 group by
-	EXTRACT(month from sc.date),
-	EXTRACT(year from sc.date)
+    extract(month from sc.sale_date)
 order by
-	month;
-
+    month;
 ---- Задание 4. Напишите запрос, который выведет список купленных машин у каждого пользователя.
 select
-	cu.name as person,
-	STRING_AGG(c.brand || ' ' || c.model, ', ') as cars
+    cu.name as person,
+    string_agg(b.brand_name || ' ' || c.model, ', ') as cars
 from car_shop.sales_clean sc
 join car_shop.customers cu on (sc.customer_id = cu.id)
-join car_shop.cars c on (sc.car_id = c.id)
+join car_shop.car_colors cc on (sc.car_color_id = cc.id)
+join car_shop.cars c on (cc.car_id = c.id)
+join car_shop.brands b on (c.brand_id = b.id)
 group by cu.name
 order by cu.name;
-
 ---- Задание 5. Наибольшая и наименьшая цены продажи авто с разбивкой по стране без скидки.
 select
-    brand_origin,
-    MAX(price) as price_max,
-    MIN(price) as price_min
-from raw_data.sales
-group by brand_origin
-order by brand_origin;
-
+    co.country_name as brand_origin,
+    round(max(sc.price / (1 - coalesce(sc.discount, 0)/100.0)), 2) as price_max,
+    round(min(sc.price / (1 - coalesce(sc.discount, 0)/100.0)), 2) as price_min
+from car_shop.sales_clean sc
+join car_shop.car_colors cc
+    on sc.car_color_id = cc.id
+join car_shop.cars c
+    on cc.car_id = c.id
+join car_shop.brands b
+    on c.brand_id = b.id
+join car_shop.countries co
+    on b.country_id = co.id
+group by co.country_name
+order by co.country_name;
 ---- Задание 6. Кол-во всех пользователей из США.
 select
     COUNT(*) as persons_from_usa_count
